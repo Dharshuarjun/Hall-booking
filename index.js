b283c0e8a28aee954688f863202776f370e357c2
dharshuarjun <dhanalakshm77@gmail.com>
2022-09-22 12:15:17 +0530

    done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const uniqid = require('uniqid');
+// import 'dotenv/config';
 
 const app = express();
 app.use(cors());
